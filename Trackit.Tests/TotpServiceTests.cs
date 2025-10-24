07ccc529893a542f40a341a35c536d49006a8564
Lana <virsen.sv@gmail.com>
2025-10-24 22:04:28 +0200

    feat(totp): add base TOTP service (generation, URI, verification)

---
+++ Trackit.Tests/TotpServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Trackit.Core.Services;
+using Xunit;
+
+namespace Trackit.Tests
+{
+    public class TotpServiceTests
+    {
+        [Fact]
+        public void GeneratedCodeShouldValidate()
+        {
+            var service = new TotpService();
+            var secret = service.GenerateSecret();
+            var valid = service.VerifyCode(secret, new OtpNet.Totp(OtpNet.Base32Encoding.ToBytes(secret)).ComputeTotp());
+            Assert.True(valid);
+        }
+    }
+}
